3d4921ca44e934f1c4f0952ae1eb22a7f05fbd5b
PATELxDEVIL <devangpatel1972003@gmail.com>
2026-01-03 03:14:52 +0530

    Delete apps.js

--- apps.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "parkvision-tech.firebaseapp.com",
-  databaseURL: "https://parkvision-tech-default-rtdb.firebaseio.com",
-  projectId: "parkvision-tech",
-  storageBucket: "parkvision-tech.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
-};

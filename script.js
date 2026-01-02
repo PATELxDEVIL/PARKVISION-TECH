b4ae9d0696ce5e89d562e5512aceaab0ee6cfbe9
PATELxDEVIL <devangpatel1972003@gmail.com>
2026-01-03 01:40:48 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "parkvision-tech.firebaseapp.com",
+  databaseURL: "https://parkvision-tech-default-rtdb.firebaseio.com",
+  projectId: "parkvision-tech",
+  storageBucket: "parkvision-tech.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
+};
